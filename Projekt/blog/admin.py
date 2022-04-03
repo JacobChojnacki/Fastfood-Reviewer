94e8842021ae2745c5f8604d18008760ea4ba03a
Jacob Chojnacki <jacobchojnacki@protonmail.com>
2022-04-03 18:00:09 +0200

    03.04.2022

---
+++ Projekt/blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Post, Comment
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish')
+    list_filter = ('author', 'publish')
+    search_field = ('title', 'body')
+    prepopulated_fields = {'slug':('title',)}
+    date_hierarchy = 'publish'
+    ordering = ('author', "publish")
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('email', 'post', 'created')
+    list_filter = ('email', 'created', 'updated')
+    search_fields = ('email','body')
---
+++ Projekt/blog/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.generic import ListView
+
+# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 8
+    template_name = 'blog/post/list.html'
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    comments = post.comments.filter(active = True)
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,'blog/post/detail.html',{"post":post, 'comments':comments,'comment_form': comment_form})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    sent = False
+    if request.method == "POST":
+        my_form = EmailPostForm(request.POST)
+        if my_form.is_valid():
+            my_form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            #trzeba bedzie dokonczyc subject, message itp
+        else:
+            my_form.errors
+    else:
+        my_form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': my_form,
+                                                    'sent': sent})
+
+    
