de06c35707ac6d5e2783fa6e850769a3f681e5bb
Jacob Chojnacki <jacobchojnacki@protonmail.com>
2022-04-03 19:14:08 +0200

    mechanizm wyszukiwania podobnych pozycji

--- Projekt/blog/views.py
+++ Projekt/blog/views.py
@@ -1,3 +1,4 @@
+from turtle import pos
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -5,6 +6,7 @@ from django.core.mail import send_mail
 from django.views.generic import ListView
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 
 # Create your views here.
 
@@ -51,7 +53,11 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request,'blog/post/detail.html',{"post":post, 'comments':comments,'comment_form': comment_form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(tags__in = post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags')[:8]
+    return render(request,'blog/post/detail.html',{"post":post, 'comments':comments,'comment_form': comment_form,
+                                                   "similar_posts":similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id)
